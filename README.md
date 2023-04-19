75fbf7e6c7a9a656fe59c36f04bba837513922eb
Larry Li <larryli@qq.com>
2023-04-19 08:54:14 +0800

    Init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# esp-offline-flasher
-Offline Flasher for ESP-chips
+# Offline Flasher for ESP-chips
+
+[中文](README_zh.md)
+
+ - Support Chips: ESP32-S2, ESP32-S3
+ - Default Board: [WEMOS S2 mini](https://www.wemos.cc/en/latest/s2/s2_mini.html)
---
+++ main/main.c
@@ -0,0 +1,159 @@
+#include <dirent.h>
+#include <errno.h>
+#include "driver/gpio.h"
+#include "esp_check.h"
+#include "tinyusb.h"
+#include "tusb_cdc_acm.h"
+#include "tusb_console.h"
+#include "tusb_msc_storage.h"
+
+static const char *TAG = "main";
+
+/* TinyUSB descriptors
+ ********************************************************************* */
+#define TUSB_DESC_TOTAL_LEN                                                    \
+    (TUD_CONFIG_DESC_LEN + TUD_CDC_DESC_LEN + TUD_MSC_DESC_LEN)
+
+enum { ITF_NUM_CDC = 0, ITF_NUM_CDC_DATA, ITF_NUM_MSC, ITF_NUM_TOTAL };
+
+enum {
+    EP_EMPTY = 0,
+    EPNUM_0_CDC_NOTIF,
+    EPNUM_0_CDC,
+    EPNUM_MSC,
+};
+
+static uint8_t const desc_configuration[] = {
+    // Config number, interface count, string index, total length, attribute,
+    // power in mA
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, TUSB_DESC_TOTAL_LEN,
+                          TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
+
+    // Interface number, string index, EP notification address and size, EP data
+    // address (out, in) and size.
+    TUD_CDC_DESCRIPTOR(ITF_NUM_CDC, 4, 0x80 | EPNUM_0_CDC_NOTIF, 8, EPNUM_0_CDC,
+                       0x80 | EPNUM_0_CDC, CFG_TUD_CDC_EP_BUFSIZE),
+
+    // Interface number, string index, EP Out & EP In address, EP size
+    TUD_MSC_DESCRIPTOR(ITF_NUM_MSC, 5, EPNUM_MSC, 0x80 | EPNUM_MSC,
+                       TUD_OPT_HIGH_SPEED ? 512 : 64),
+};
+
+static tusb_desc_device_t descriptor_config = {
+    .bLength = sizeof(descriptor_config),
+    .bDescriptorType = TUSB_DESC_DEVICE,
+    .bcdUSB = 0x0200,
+    .bDeviceClass = TUSB_CLASS_MISC,
+    .bDeviceSubClass = MISC_SUBCLASS_COMMON,
+    .bDeviceProtocol = MISC_PROTOCOL_IAD,
+    .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
+    .idVendor = USB_ESPRESSIF_VID, // This is Espressif VID. This needs to be
+                                   // changed according to Users / Customers
+    .idProduct = 0x4003,
+    .bcdDevice = CONFIG_TINYUSB_DESC_BCD_DEVICE,
+    .iManufacturer = 0x01,
+    .iProduct = 0x02,
+    .iSerialNumber = 0x03,
+    .bNumConfigurations = 0x01};
+
+static char const *string_desc_arr[] = {
+    // array of pointer to string descriptors
+    (char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
+    CONFIG_TINYUSB_DESC_MANUFACTURER_STRING, // 1: Manufacturer
+    CONFIG_TINYUSB_DESC_PRODUCT_STRING,      // 2: Product
+    CONFIG_TINYUSB_DESC_SERIAL_STRING,       // 3: Serials, should use chip ID
+    CONFIG_TINYUSB_DESC_CDC_STRING,          // 4: CDC Interface
+    CONFIG_TINYUSB_DESC_MSC_STRING,          // 5: MSC Interface
+    NULL // NULL: Must be last. Indicates end of array
+};
+/********************************* TinyUSB descriptors ************/
+
+// mount the partition and show all the files in BASE_PATH
+static void _mount(void)
+{
+    ESP_LOGI(TAG, "Mount storage...");
+    ESP_ERROR_CHECK(tinyusb_msc_storage_mount(CONFIG_TINYUSB_MSC_MOUNT_PATH));
+
+    // List all the files in this directory
+    ESP_LOGI(TAG, "\nls command output:");
+    struct dirent *d;
+    DIR *dh = opendir(CONFIG_TINYUSB_MSC_MOUNT_PATH);
+    if (!dh) {
+        if (errno == ENOENT) {
+            // If the directory is not found
+            ESP_LOGE(TAG, "Directory doesn't exist %s", CONFIG_TINYUSB_MSC_MOUNT_PATH);
+        } else {
+            // If the directory is not readable then throw error and exit
+            ESP_LOGE(TAG, "Unable to read directory %s", CONFIG_TINYUSB_MSC_MOUNT_PATH);
+        }
+        return;
+    }
+    // While the next entry is not readable we will print directory files
+    while ((d = readdir(dh)) != NULL) {
+        printf("%s\n", d->d_name);
+    }
+    return;
+}
+
+static esp_err_t storage_init_spiflash(wl_handle_t *wl_handle)
+{
+    ESP_LOGI(TAG, "Initializing wear levelling");
+
+    const esp_partition_t *data_partition = esp_partition_find_first(
+        ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_FAT, NULL);
+    if (data_partition == NULL) {
+        ESP_LOGE(TAG,
+                 "Failed to find FATFS partition. Check the partition table.");
+        return ESP_ERR_NOT_FOUND;
+    }
+
+    return wl_mount(data_partition, wl_handle);
+}
+
+static void storage_mount_changed(tinyusb_msc_event_t *event)
+{
+    if (event->mount_changed_data.is_mounted) {
+        ESP_LOGI(TAG, "Storage mounted");
+        gpio_set_level(CONFIG_LED_GPIO, 0);
+    } else {
+        ESP_LOGI(TAG, "Storage unmounted");
+        gpio_set_level(CONFIG_LED_GPIO, 1);
+    }
+}
+
+void app_main(void)
+{
+    gpio_reset_pin(CONFIG_LED_GPIO);
+    gpio_set_direction(CONFIG_LED_GPIO, GPIO_MODE_OUTPUT);
+
+    ESP_LOGI(TAG, "Initializing storage...");
+
+    static wl_handle_t wl_handle = WL_INVALID_HANDLE;
+    ESP_ERROR_CHECK(storage_init_spiflash(&wl_handle));
+
+    const tinyusb_msc_spiflash_config_t config_spi = {
+        .wl_handle = wl_handle,
+        .callback_mount_changed = storage_mount_changed};
+    ESP_ERROR_CHECK(tinyusb_msc_storage_init_spiflash(&config_spi));
+
+    // mounted in the app by default
+    _mount();
+
+    ESP_LOGI(TAG, "USB initialization");
+    const tinyusb_config_t tusb_cfg = {
+        .self_powered = false,
+        .device_descriptor = &descriptor_config,
+        .string_descriptor = string_desc_arr,
+        .string_descriptor_count =
+            sizeof(string_desc_arr) / sizeof(string_desc_arr[0]),
+        .external_phy = false,
+        .configuration_descriptor = desc_configuration,
+    };
+    ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
+    tinyusb_config_cdcacm_t acm_cfg = {
+        0}; // the configuration uses default values
+    ESP_ERROR_CHECK(tusb_cdc_acm_init(&acm_cfg));
+
+    esp_tusb_init_console(TINYUSB_CDC_ACM_0); // log to usb
+    ESP_LOGI(TAG, "USB initialization DONE");
+}
